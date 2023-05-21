0ec757cc7b2642d24a17ea5b6234f05c2e1e23ee
Aryan Singh <114330931+aryanas159@users.noreply.github.com>
2023-05-21 12:08:57 +0530

    port changed

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ mongoose.connection.once("open", () => {
 	app.listen(PORT, () => console.log(`server connected to port: ${PORT}`));
 });
 
-const wss = new ws.WebSocketServer({ port: 8080 }); //New WebSocket defined
+const wss = new ws.WebSocketServer({ port: 8086 }); //New WebSocket defined
 
 wss.on("connection", (connection, req) => {
 	const notifyAboutOnlinePeople = () => {
